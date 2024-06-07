5ed4c107e3a9477072de4bce39b62dd86b9829b3
Chasles Maxime <maxime.chasles@etudiant.univ-lr.fr>
2024-06-08 01:50:51 +0200

    navigation

--- js/main.js
+++ js/main.js
@@ -1,44 +1,92 @@
-const burgerBtn = document.querySelector('.burger');
-const menuLinks = document.querySelectorAll('.main_menu_links li');
+//*** Gestion de la navigation ***//
 
-
-if (window.innerWidth > 768) {
-    document.addEventListener('DOMContentLoaded', slideUpLinks);
-    window.addEventListener('scroll', slideUpLinks)
-} else {
-    document.removeEventListener('DOMContentLoaded', slideUpLinks);
-    window.removeEventListener('scroll', slideUpLinks)
-
-}
-
-
-
-burgerBtn.addEventListener('click', extendMenu);
-
-
-function slideUpLinks(e) {
-    console.log(window.innerWidth)
-    if (window.scrollY > window.innerHeight) {
-        menuLinks.forEach(ml => ml.classList.remove('active'))
-
-    } else if (window.scrollY <= 50) {
-        menuLinks.forEach(ml => ml.classList.add('active'))
-    } 
-}
-
-
-function extendMenu(e) {
+/**
+ * 
+ */
+class Navigation {
     /**
-     * TODO: Traitement du burger
+     * 
+     * @param {*} id 
      */
+    constructor(id) {
+        this.element = document.querySelector(`#${id}`);
+        this.links = [...this.element.getElementsByTagName('a')];
+        this.button = document.querySelector('.burger');
+        this.pages = [...document.getElementsByClassName('page')].slice(1);
 
-    // Affichage du menu principal
-    const menu = document.querySelector('.main_menu_links');
-    menu.classList.toggle('active');
-    menuLinks.forEach(menuLink => {
-        setTimeout(() => {
-            menuLink.classList.toggle('active')
+        this.button.addEventListener('click', this.extendMenu.bind(this));
+        this.links.forEach(link => link.addEventListener('click', this.navigate.bind(this)));
+        window.addEventListener('scroll', this.fadeInContents.bind(this));
+        window.addEventListener('DOMContentLoaded', this.slideUpLinks.bind(this))
+        window.addEventListener('scroll', this.slideUpLinks.bind(this));
 
-        }, 750)
+    }
+
+    /**
+     * 
+     * @param {PointerEvent} e 
+     */
+    navigate(e) {
+        e.preventDefault();
+        const page = e.currentTarget.dataset.page;
+        const targetY = window.innerHeight * page;
         
-})}
+        window.scrollTo({
+            left: 0,
+            top: targetY,
+            behavior: "smooth"
+        })
+    }
+
+    extendMenu(e) {
+
+        /**
+         * TODO: Traitement du burger
+         */
+    
+        // Affichage du menu principal
+        const menu = document.querySelector('.main_menu_links');
+        if (menu.classList.contains('active')) {
+            menu.classList.remove('active');
+            setTimeout(() => {
+                menu.style.display = 'none';
+            }, 1000 )
+            
+        } else {
+            menu.style.display = 'flex';
+            setTimeout(() => {
+                menu.classList.add('active')
+            }, 100 )
+
+        }
+        
+        // setTimeout(() => {
+        //     menu.classList.toggle('active')
+        // }, 100 )
+        // menu.classList.toggle('active');
+            
+    }
+
+    fadeInContents(e) {
+        this.pages.forEach(page => {
+            const article = page.querySelector('article');
+            const top = page.getBoundingClientRect().top;
+            if (top / innerHeight * 100 < 75 && top + innerHeight > 0) {
+                article.classList.add('visible')
+            } else {
+                article.classList.remove('visible')
+            }
+        })
+    }
+
+    slideUpLinks(e) {
+        if (window.scrollY > window.innerHeight) {
+            this.links.forEach(link => link.parentElement.classList.remove('active'))
+    
+        } else if (window.scrollY <= 50) {
+            this.links.forEach(link => link.parentElement.classList.add('active'))
+        } 
+    }
+}
+
+const nav = new Navigation('main-nav');
